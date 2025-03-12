9e0ca2b3a8c58008de947d17b8a34cae58d32c83
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-03-12 17:09:53 +0100

    use login logic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,29 @@
-export default function LoginPage() {
+import { redirect } from 'next/navigation';
+import { logIn } from '@/actions';
+import { getSession } from '@/lib';
+
+export default async function LoginPage() {
+  const session = await getSession();
+
+  if (session) {
+    redirect('/');
+  }
+
   return (
-    <form className='flex flex-col gap-2'>
-      <input type='email' name='email' placeholder='Email' />
-      <input type='password' name='password' placeholder='*****' />
-      <button>Login</button>
+    <form action={logIn} className='flex flex-col gap-2'>
+      <input
+        type='email'
+        name='email'
+        placeholder='Email'
+        className='rounded bg-white px-2 py-1 text-base text-black'
+      />
+      <input
+        type='password'
+        name='password'
+        placeholder='********'
+        className='rounded bg-white px-2 py-1 text-base text-black'
+      />
+      <button className='mt-4 rounded border-2 border-white p-1'>Login</button>
     </form>
   );
 }
