9b69dd18f59dd57dfdbb011345a8840de97c00ec
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-03-12 17:10:30 +0100

    show session status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function HomePage() {
-  return <h1 className='text-center text-2xl font-bold'>Basic Auth</h1>;
+import { getSession } from '@/lib';
+
+export default async function HomePage() {
+  const session = await getSession();
+
+  console.log(session);
+
+  return (
+    <div className='text-center'>
+      <h1 className='text-2xl font-bold'>Basic Auth</h1>
+      <p>{session ? 'You are logged in' : 'You are not logged in'}</p>
+    </div>
+  );
 }
