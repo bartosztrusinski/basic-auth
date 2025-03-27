d0f23131540c7c3e62d4d15801939e181f3a99a9
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-03-27 17:17:57 +0100

    fetch new access token on app mount

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -1,16 +1,20 @@
 'use client';
 
+import { refreshAccessToken } from '@/refresh-access-token';
 import {
   createContext,
   useContext,
+  useEffect,
   useState,
   type Dispatch,
   type ReactNode,
   type SetStateAction,
 } from 'react';
 
+type AccessToken = string | null;
+
 const AuthContext = createContext<{
-  accessToken: string | null;
+  accessToken: AccessToken;
   setAccessToken: Dispatch<SetStateAction<string | null>>;
 }>({
   accessToken: null,
@@ -18,7 +22,27 @@ const AuthContext = createContext<{
 });
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [accessToken, setAccessToken] = useState<AccessToken>(null);
+  const [isInitializing, setIsInitializing] = useState(true);
+
+  useEffect(() => {
+    const getNewToken = async () => {
+      try {
+        const newToken = await refreshAccessToken();
+        setAccessToken(newToken);
+      } catch {
+        console.error('Failed to refresh token');
+      } finally {
+        setIsInitializing(false);
+      }
+    };
+
+    void getNewToken();
+  }, []);
+
+  if (isInitializing) {
+    return null;
+  }
 
   return (
     <AuthContext.Provider value={{ accessToken, setAccessToken }}>{children}</AuthContext.Provider>
