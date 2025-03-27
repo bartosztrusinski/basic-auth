b69c00e560e5fa7eac354ed3cc56a11bb3d24443
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-03-27 19:18:36 +0100

    add secret env var for refresh tokens

--- src/env.js
+++ src/env.js
@@ -4,14 +4,16 @@ import { z } from 'zod';
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']),
-    SECRET: z.string(),
+    ACCESS_TOKEN_SECRET: z.string(),
+    REFRESH_TOKEN_SECRET: z.string(),
     ACCESS_TOKEN_EXPIRATION_TIME: z.coerce.number(),
     REFRESH_TOKEN_EXPIRATION_TIME: z.coerce.number(),
   },
   client: {},
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    SECRET: process.env.SECRET,
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
     ACCESS_TOKEN_EXPIRATION_TIME: process.env.ACCESS_TOKEN_EXPIRATION_TIME,
     REFRESH_TOKEN_EXPIRATION_TIME: process.env.REFRESH_TOKEN_EXPIRATION_TIME,
   },
