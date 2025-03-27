c6c01b03aced0d64342262bfd8b4c74b3cdb3736
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-03-27 19:20:03 +0100

    use auth provider and header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
-import Link from 'next/link';
 import { type Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
-import { LogoutButton } from '@/components/logout-button';
-import { getSession } from '@/lib';
+import { AuthProvider } from '@/hooks/use-auth';
+import { Header } from '@/components/header';
 import '@/globals.css';
 
 export const metadata: Metadata = {
@@ -12,40 +11,13 @@ export const metadata: Metadata = {
 };
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  const session = await getSession();
-
   return (
     <html lang='en' className={`${GeistSans.variable}`}>
       <body className='bg-zinc-900 text-zinc-50'>
-        <nav className='flex justify-between gap-4 bg-zinc-800 px-8 py-4 text-zinc-50'>
-          <ul className='flex gap-8'>
-            <li>
-              <Link href='/'>Home</Link>
-            </li>
-          </ul>
-          <ul className='flex gap-8'>
-            {session ? (
-              <>
-                <li>
-                  <Link href='/profile'>Profile</Link>
-                </li>
-                <li>
-                  <LogoutButton />
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <Link href='/login'>Log In</Link>
-                </li>
-                <li>
-                  <Link href='/register'>Sign Up</Link>
-                </li>
-              </>
-            )}
-          </ul>
-        </nav>
-        <main className='mx-auto mt-4 w-full max-w-80 p-4'>{children}</main>
+        <AuthProvider>
+          <Header />
+          <main className='mx-auto mt-4 w-full max-w-80 p-4'>{children}</main>
+        </AuthProvider>
       </body>
     </html>
   );
