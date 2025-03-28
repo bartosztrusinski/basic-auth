d7bdbaddba8f71e1fdda8d7ee4c7649eca2799b3
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-03-28 01:21:18 +0100

    create static about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div className='text-center'>
+      <h1 className='text-2xl font-bold'>About</h1>
+      <p className='break-words'>This is the about page</p>
+    </div>
+  );
+}
