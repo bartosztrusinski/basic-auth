007833b88cafa366eec4a5cbaee819a852d171eb
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-03-30 17:32:26 +0200

    create log in action

--- src/actions.ts
+++ src/actions.ts
@@ -1,14 +1,52 @@
 'use server';
 
 import { redirect } from 'next/navigation';
-import { generateSalt, hashPassword } from '@/auth/password';
+import { comparePasswords, generateSalt, hashPassword } from '@/auth/password';
 import { createUserSession } from '@/auth/session';
 import { db } from '@/db';
-import { signupSchema } from '@/schemas';
+import { loginSchema, signupSchema } from '@/schemas';
 
 type ActionState = {
   errors?: string[];
 };
+
+export async function logIn(_: ActionState, formData: FormData): Promise<ActionState> {
+  const { data, error } = loginSchema.safeParse(Object.fromEntries(formData.entries()));
+
+  if (error) {
+    return {
+      errors: error.errors.map((err) => err.message),
+    };
+  }
+
+  const { email, password } = data;
+
+  try {
+    const user = await db.getUserByEmail(email);
+
+    if (!user) {
+      return {
+        errors: ['Invalid email or password'],
+      };
+    }
+
+    const isCorrectPassword = await comparePasswords(password, user.password, user.salt);
+
+    if (!isCorrectPassword) {
+      return {
+        errors: ['Invalid email or password'],
+      };
+    }
+
+    await createUserSession(user);
+  } catch (error) {
+    return {
+      errors: [error instanceof Error ? error.message : 'An unknown error occurred'],
+    };
+  }
+
+  redirect('/');
+}
 
 export async function signUp(_: ActionState, formData: FormData): Promise<ActionState> {
   const { data, error } = signupSchema.safeParse(Object.fromEntries(formData.entries()));
