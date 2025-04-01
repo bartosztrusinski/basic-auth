2c7f69e348c9fba27530782e6fb39ecb29286dd0
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-02 01:51:15 +0200

    add auth pre-checks in middleware

---
+++ src/auth/middleware.ts
@@ -0,0 +1,19 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+// TODO create auth config
+const privateRoutes = ['/private', '/profile'];
+const adminRoutes = ['/admin'];
+const SESSION_COOKIE_KEY = 'session-id';
+
+export function authMiddleware(request: NextRequest): NextResponse {
+  const isAuthenticated = request.cookies.has(SESSION_COOKIE_KEY);
+  const isProtectedRoute = [...privateRoutes, ...adminRoutes].some((route) =>
+    request.nextUrl.pathname.startsWith(route),
+  );
+
+  if (isProtectedRoute && !isAuthenticated) {
+    return NextResponse.redirect(new URL('/log-in', request.url));
+  }
+
+  return NextResponse.next();
+}
---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { type NextRequest } from 'next/server';
+import { authMiddleware } from '@/auth/middleware';
+
+export function middleware(request: NextRequest) {
+  return authMiddleware(request);
+}
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
