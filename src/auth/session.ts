9cf80704bdba2c793121ca02c0054ea1f3ded5fe
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-01 19:28:49 +0200

    add session delete functionality

--- src/auth/session.ts
+++ src/auth/session.ts
@@ -4,35 +4,11 @@ import { randomBytes } from 'node:crypto';
 import { env } from '@/env';
 import { db, type User } from '@/db';
 
-type UserSession = Pick<User, 'id' | 'role'>;
+export type UserSession = Pick<User, 'id' | 'role'>;
 
 const SESSION_COOKIE_KEY = 'session-id';
 
-export async function createUserSession(user: UserSession) {
-  const sessionId = randomBytes(512).toString('hex');
-
-  await db.createSession({
-    id: sessionId,
-    userId: user.id,
-    userRole: user.role,
-    expirationTime: Date.now() + env.SESSION_EXPIRATION_IN_SECONDS * 1000,
-  });
-
-  await setSessionCookie(sessionId);
-}
-
-export async function setSessionCookie(sessionId: string) {
-  const cookieStore = await cookies();
-  cookieStore.set(SESSION_COOKIE_KEY, sessionId, {
-    secure: true,
-    httpOnly: true,
-    sameSite: 'lax',
-    maxAge: env.SESSION_EXPIRATION_IN_SECONDS,
-    path: '/',
-  });
-}
-
-export const getUserFromSession = cache(async (): Promise<UserSession | null> => {
+export const getUserSession = cache(async (): Promise<UserSession | null> => {
   const cookieStore = await cookies();
   const sessionId = cookieStore.get(SESSION_COOKIE_KEY)?.value;
 
@@ -51,3 +27,39 @@ export const getUserFromSession = cache(async (): Promise<UserSession | null> =>
     role: session.userRole,
   };
 });
+
+export async function createUserSession(user: UserSession) {
+  const sessionId = randomBytes(512).toString('hex');
+
+  await db.createSession({
+    id: sessionId,
+    userId: user.id,
+    userRole: user.role,
+    expirationTime: Date.now() + env.SESSION_EXPIRATION_IN_SECONDS * 1000,
+  });
+
+  await setSessionCookie(sessionId);
+}
+
+export async function deleteUserSession() {
+  const cookieStore = await cookies();
+  const sessionId = cookieStore.get(SESSION_COOKIE_KEY)?.value;
+
+  if (!sessionId) {
+    return;
+  }
+
+  await db.deleteSession(sessionId);
+  cookieStore.delete(SESSION_COOKIE_KEY);
+}
+
+export async function setSessionCookie(sessionId: string) {
+  const cookieStore = await cookies();
+  cookieStore.set(SESSION_COOKIE_KEY, sessionId, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'lax',
+    maxAge: env.SESSION_EXPIRATION_IN_SECONDS,
+    path: '/',
+  });
+}
