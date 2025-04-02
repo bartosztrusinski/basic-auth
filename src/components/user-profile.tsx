0e446e0714b1e1682c05cc5e991e6a9395519bbe
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-02 20:52:25 +0200

    add role edit, use action, show success and error messages

--- src/components/user-profile.tsx
+++ src/components/user-profile.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { useState } from 'react';
+import { useActionState, useEffect, useState } from 'react';
 import type { FullUser } from '@/auth/session';
+import { editProfile } from '@/actions';
 
 type Props = {
   user: FullUser;
@@ -9,41 +10,78 @@ type Props = {
 
 export function UserProfile({ user }: Props) {
   const [isEditing, setIsEditing] = useState(false);
+  const [state, action, isPending] = useActionState(editProfile, {});
+
+  useEffect(() => {
+    if (state.success) {
+      setIsEditing(false);
+    }
+  }, [state]);
 
   return (
     <>
       <h1 className='mb-6 text-4xl font-bold'>Your Profile</h1>
-      <p className='mb-2'>
-        Email: <strong className='text-lg'>{user.email}</strong>
-      </p>
-      {isEditing ? (
-        <form>
-          Name:{' '}
-          <input
-            name='name'
-            required
-            placeholder='Name'
-            defaultValue={user.name}
-            className='rounded bg-white px-2 py-1 text-base text-black'
-          />
-          {/* <p className='min-h-6 text-red-500'> {state?.error && state.error}</p>
-          <button disabled={isPending} className='rounded border-2 border-white p-1'>
-            {isPending ? 'Saving...' : 'Save'}
-          </button> */}
-        </form>
-      ) : (
-        <>
-          <p>
-            Name: <strong className='text-lg'>{user.name}</strong>
-          </p>
-          <button
-            onClick={() => setIsEditing(true)}
-            className='mt-4 rounded border-2 border-white p-1'
-          >
-            Edit Profile
-          </button>
-        </>
-      )}
+      <section className='mb-6 space-y-5 rounded-lg border border-gray-500 p-6'>
+        <p>
+          Email: <strong className='text-lg'>{user.email}</strong>
+        </p>
+        {isEditing ? (
+          <form action={action} className='space-y-5'>
+            <div className='flex items-center gap-2'>
+              <label htmlFor='name'>Name:</label>
+              <input
+                id='name'
+                name='name'
+                required
+                placeholder='Name'
+                defaultValue={user.name}
+                className='min-w-0 grow rounded bg-white px-2 py-1 text-base text-black'
+              />
+            </div>
+            <div className='flex items-center gap-2'>
+              <label htmlFor='role'>Role:</label>
+              <select
+                id='role'
+                name='role'
+                defaultValue={user.role}
+                className='grow rounded bg-white px-2 py-1 text-base text-black'
+              >
+                <option value='admin'>admin</option>
+                <option value='user'>user</option>
+              </select>
+            </div>
+            {state.errors && (
+              <div className='text-sm font-light text-red-500'>
+                {state.errors.map((error, index) => (
+                  <p key={index}>{error}</p>
+                ))}
+              </div>
+            )}
+
+            <button disabled={isPending} className='w-full rounded border-2 border-white p-1'>
+              {isPending ? 'Saving...' : 'Save'}
+            </button>
+          </form>
+        ) : (
+          <>
+            <p>
+              Name: <strong className='text-lg'>{user.name}</strong>
+            </p>
+            <p>
+              Role: <strong className='text-lg'>{user.role}</strong>
+            </p>
+            {state.success && (
+              <div className='text-sm font-light text-green-500'>Profile updated successfully!</div>
+            )}
+            <button
+              onClick={() => setIsEditing(true)}
+              className='w-full rounded border-2 border-white p-1'
+            >
+              Edit Profile
+            </button>
+          </>
+        )}
+      </section>
     </>
   );
 }
