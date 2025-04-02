e9165df61e715d324bedc7ca25fd203210ebdf9c
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-03 00:31:28 +0200

    create public about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+  return (
+    <div className='text-center'>
+      <h1 className='mb-2 text-2xl font-bold'>About</h1>
+      <p>
+        This is a public route that can be accessed by anyone. It does not require authentication.
+      </p>
+    </div>
+  );
+}
