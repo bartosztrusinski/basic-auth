d0febd9b8c40a7c332c3e33939d3095feb43f265
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-03 02:08:22 +0200

    add links to new pages

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,14 +2,24 @@ import Link from 'next/link';
 import { getUserSession } from '@/auth/session';
 import { LogoutButton } from '@/components/logout-button';
 
+// TODO fix stale ui after session expiry
 export async function Header() {
   const user = await getUserSession();
 
   return (
-    <nav className='flex items-center justify-between gap-4 bg-zinc-800 px-8 py-4 text-zinc-50'>
+    <nav className='flex flex-wrap justify-between gap-x-8 gap-y-4 bg-zinc-800 p-4 text-zinc-50'>
       <ul className='flex gap-8'>
         <li>
           <Link href='/'>Home</Link>
+        </li>
+        <li>
+          <Link href='/about'>About</Link>
+        </li>
+        <li>
+          <Link href='/private'>Private</Link>
+        </li>
+        <li>
+          <Link href='/admin'>Admin</Link>
         </li>
       </ul>
       <ul className='flex gap-8'>
