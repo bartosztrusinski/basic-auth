a057f1472c836697bce3a409de172a26c792e37f
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-03 02:51:13 +0200

    redirect from auth page when logged in

--- src/auth/middleware.ts
+++ src/auth/middleware.ts
@@ -4,14 +4,20 @@ import { type NextRequest, NextResponse } from 'next/server';
 // TODO create auth config
 const privateRoutes = ['/private', '/profile'];
 const adminRoutes = ['/admin'];
+const authRoutes = ['/log-in', '/sign-up'];
 const SESSION_COOKIE_KEY = 'session-id';
 
 export function authMiddleware(request: NextRequest): NextResponse {
   const { pathname } = request.nextUrl;
   const isAuthenticated = request.cookies.has(SESSION_COOKIE_KEY);
+  const isAuthRoute = authRoutes.some((route) => pathname.startsWith(route));
   const isProtectedRoute = [...privateRoutes, ...adminRoutes].some((route) =>
     pathname.startsWith(route),
   );
+
+  if (isAuthRoute && isAuthenticated) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
 
   if (isProtectedRoute && !isAuthenticated) {
     return NextResponse.redirect(
