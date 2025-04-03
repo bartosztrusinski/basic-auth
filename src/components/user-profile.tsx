0382d8692755ef974bc79c87f838e321b7612aef
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-03 02:10:22 +0200

    adjust styles and add cancel button

--- src/components/user-profile.tsx
+++ src/components/user-profile.tsx
@@ -19,69 +19,86 @@ export function UserProfile({ user }: Props) {
   }, [state]);
 
   return (
-    <>
-      <h1 className='mb-6 text-4xl font-bold'>Your Profile</h1>
-      <section className='mb-6 space-y-5 rounded-lg border border-gray-500 p-6'>
-        <p>
-          Email: <strong className='text-lg'>{user.email}</strong>
-        </p>
-        {isEditing ? (
-          <form action={action} className='space-y-5'>
-            <div className='flex items-center gap-2'>
-              <label htmlFor='name'>Name:</label>
-              <input
-                id='name'
-                name='name'
-                required
-                placeholder='Name'
-                defaultValue={user.name}
-                className='min-w-0 grow rounded bg-white px-2 py-1 text-base text-black'
-              />
-            </div>
-            <div className='flex items-center gap-2'>
-              <label htmlFor='role'>Role:</label>
-              <select
-                id='role'
-                name='role'
-                defaultValue={user.role}
-                className='grow rounded bg-white px-2 py-1 text-base text-black'
-              >
-                <option value='admin'>admin</option>
-                <option value='user'>user</option>
-              </select>
-            </div>
-            {state.errors && (
-              <div className='text-sm font-light text-red-500'>
-                {state.errors.map((error, index) => (
-                  <p key={index}>{error}</p>
-                ))}
-              </div>
-            )}
+    <div className='space-y-4 rounded-lg border border-zinc-600 p-6'>
+      <p>
+        <strong>Email:</strong> {user.email}
+      </p>
+      {isEditing ? (
+        <form action={action} className='space-y-4'>
+          <div className='flex items-center gap-2'>
+            <label htmlFor='name'>
+              <strong>Name:</strong>
+            </label>
+            <input
+              id='name'
+              name='name'
+              required
+              placeholder='Name'
+              defaultValue={user.name}
+              className='h-6 grow rounded bg-white px-1 text-base text-black'
+            />
+          </div>
+          <div className='flex items-center gap-2'>
+            <label htmlFor='role'>
+              <strong>Role:</strong>
+            </label>
+            <select
+              id='role'
+              name='role'
+              defaultValue={user.role}
+              className='h-6 grow rounded bg-white px-1 text-base text-black'
+            >
+              <option value='admin'>admin</option>
+              <option value='user'>user</option>
+            </select>
+          </div>
 
-            <button disabled={isPending} className='w-full rounded border-2 border-white p-1'>
+          {state.errors && (
+            <div className='text-sm font-light text-red-500'>
+              {state.errors.map((error, index) => (
+                <p key={index}>{error}</p>
+              ))}
+            </div>
+          )}
+
+          <div className='flex gap-2'>
+            <button
+              type='submit'
+              disabled={isPending}
+              className='grow rounded border-2 border-white p-1'
+            >
               {isPending ? 'Saving...' : 'Save'}
             </button>
-          </form>
-        ) : (
-          <>
-            <p>
-              Name: <strong className='text-lg'>{user.name}</strong>
-            </p>
-            <p>
-              Role: <strong className='text-lg'>{user.role}</strong>
-            </p>
-            {state.success && (
-              <div className='text-sm font-light text-green-500'>Profile updated successfully!</div>
-            )}
             <button
-              onClick={() => setIsEditing(true)}
-              className='w-full rounded border-2 border-white p-1'
+              type='button'
+              onClick={() => setIsEditing(false)}
+              className='w-9 rounded border-2 border-white font-bold'
             >
-              Edit Profile
+              ⨉
             </button>
-          </>
-        )}
-      </section>
-    </>
+          </div>
+        </form>
+      ) : (
+        <>
+          <p>
+            <strong>Name:</strong> {user.name}
+          </p>
+          <p>
+            <strong>Role:</strong> {user.role}
+          </p>
+
+          {state.success && (
+            <p className='text-sm font-light text-green-500'>Profile updated successfully!</p>
+          )}
+
+          <button
+            onClick={() => setIsEditing(true)}
+            className='w-full rounded border-2 border-white p-1'
+          >
+            Edit Profile
+          </button>
+        </>
+      )}
+    </div>
   );
 }
