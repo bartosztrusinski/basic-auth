8228c8af77372ff453242d60418d95a06bea2a3b
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-04 21:00:09 +0200

    add control flow component to protect content

---
+++ src/auth/components/protect.tsx
@@ -0,0 +1,19 @@
+import { type ReactNode } from 'react';
+import { type User } from '@/db';
+import { auth } from '@/auth/session';
+
+type Props = {
+  children: ReactNode;
+  fallback?: ReactNode;
+  role?: User['role'];
+};
+
+export async function Protect({ children, fallback, role }: Props) {
+  const { userId, userRole } = await auth();
+
+  if (!userId || userRole !== role) {
+    return fallback ? <>{fallback}</> : null;
+  }
+
+  return <>{children}</>;
+}
