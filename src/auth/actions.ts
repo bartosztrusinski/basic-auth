aa0922d8748437061879bfb841dd0e6a32d21bb2
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-11 02:44:33 +0200

    create file for auth actions

---
+++ src/auth/actions.ts
@@ -0,0 +1,62 @@
+'use server';
+
+import { db, type Session } from '@/db';
+import { loginSchema } from '@/schemas';
+import { createUserSession, deleteUserSession } from './session';
+import { comparePasswords } from './password';
+
+export type ActionState<T extends Record<string, unknown> = Record<string, unknown>> = {
+  isSuccess: boolean;
+  errors?: string[];
+} & T;
+
+type LoginActionState = ActionState<{ session?: Session }>;
+
+export async function logIn(_: LoginActionState, formData: FormData): Promise<LoginActionState> {
+  const { data, error } = loginSchema.safeParse(Object.fromEntries(formData.entries()));
+
+  if (error) {
+    return {
+      isSuccess: false,
+      errors: error.errors.map((err) => err.message),
+    };
+  }
+
+  const { email, password } = data;
+
+  try {
+    const user = await db.getUserByEmail(email);
+
+    if (!user) {
+      return {
+        isSuccess: false,
+        errors: ['Invalid email or password'],
+      };
+    }
+
+    const isCorrectPassword = await comparePasswords(password, user.password, user.salt);
+
+    if (!isCorrectPassword) {
+      return {
+        isSuccess: false,
+        errors: ['Invalid email or password'],
+      };
+    }
+
+    const session = await createUserSession({ userId: user.id, userRole: user.role });
+
+    return {
+      isSuccess: true,
+      session,
+    };
+  } catch (error) {
+    return {
+      isSuccess: false,
+      errors: [error instanceof Error ? error.message : 'An unknown error occurred'],
+    };
+  }
+}
+
+export async function logOut() {
+  await deleteUserSession();
+}
