28516e2681875ef9b52ffa0da280444106013280
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-11 03:11:46 +0200

    redirect in actions when unauthenticated

--- src/actions.ts
+++ src/actions.ts
@@ -1,65 +1,17 @@
 'use server';
 
-import { revalidatePath } from 'next/cache';
-import { comparePasswords, generateSalt, hashPassword } from '@/auth/password';
-import { createUserSession, deleteUserSession, auth, updateUserSession } from '@/auth/session';
-import { db, type Session } from '@/db';
-import { editProfileSchema, loginSchema, signupSchema } from '@/schemas';
-
-type ActionState = {
-  success?: boolean;
-  errors?: string[];
-};
-
-export async function logIn(
-  _: ActionState,
-  formData: FormData,
-): Promise<ActionState & { session?: Session }> {
-  const { data, error } = loginSchema.safeParse(Object.fromEntries(formData.entries()));
-
-  if (error) {
-    return {
-      errors: error.errors.map((err) => err.message),
-    };
-  }
-
-  const { email, password } = data;
-
-  try {
-    const user = await db.getUserByEmail(email);
-
-    if (!user) {
-      return {
-        errors: ['Invalid email or password'],
-      };
-    }
-
-    const isCorrectPassword = await comparePasswords(password, user.password, user.salt);
-
-    if (!isCorrectPassword) {
-      return {
-        errors: ['Invalid email or password'],
-      };
-    }
-
-    const session = await createUserSession({ userId: user.id, userRole: user.role });
-
-    return {
-      success: true,
-      session,
-    };
-  } catch (error) {
-    return {
-      errors: [error instanceof Error ? error.message : 'An unknown error occurred'],
-    };
-  }
-}
+import { generateSalt, hashPassword } from '@/auth/password';
+import { createUserSession, auth, updateUserSession } from '@/auth/session';
+import { type ActionState } from '@/auth/actions';
+import { db } from '@/db';
+import { editProfileSchema, signupSchema } from '@/schemas';
 
 export async function signUp(_: ActionState, formData: FormData): Promise<ActionState> {
   const { data, error } = signupSchema.safeParse(Object.fromEntries(formData.entries()));
 
   if (error) {
     return {
+      isSuccess: false,
       errors: error.errors.map((err) => err.message),
     };
   }
@@ -69,69 +21,49 @@ export async function signUp(_: ActionState, formData: FormData): Promise<Action
   try {
     const salt = generateSalt();
     const hashedPassword = await hashPassword(password, salt);
-
-    const user = await db.createUser({
-      email,
-      name,
-      password: hashedPassword,
-      salt,
-    });
-
-    if (!user) {
-      throw new Error('User creation failed');
-    }
-
+    const user = await db.createUser({ email, name, password: hashedPassword, salt });
     await createUserSession({ userId: user.id, userRole: user.role });
+
+    return {
+      isSuccess: true,
+    };
   } catch (error) {
     return {
+      isSuccess: false,
       errors: [error instanceof Error ? error.message : 'An unknown error occurred'],
     };
   }
-
-  return {};
 }
 
-export async function logOut(_: ActionState): Promise<ActionState> {
-  await deleteUserSession();
-
-  return {
-    success: true,
-  };
-}
-
-export async function editProfile(
-  formData: FormData,
-): Promise<ActionState & { isUnauthenticated?: boolean }> {
-  const { data, error } = editProfileSchema.safeParse(Object.fromEntries(formData.entries()));
+export async function editProfile(_: ActionState, formData: FormData): Promise<ActionState> {
+  const { data: userData, error } = editProfileSchema.safeParse(
+    Object.fromEntries(formData.entries()),
+  );
 
   if (error) {
     return {
+      isSuccess: false,
       errors: error.errors.map((err) => err.message),
     };
   }
 
-  const { userId } = await auth();
+  const { userId, redirectToLogin } = await auth();
 
   if (!userId) {
-    revalidatePath('/');
-    return {
-      errors: ['User not authenticated'],
-      isUnauthenticated: true,
-    };
+    return redirectToLogin('/profile');
   }
 
-  const { name, role } = data;
-
   try {
-    const { role: newRole } = await db.updateUser(userId, { name, role });
-    await updateUserSession({ userId, userRole: newRole });
+    const { role } = await db.updateUser(userId, userData);
+    await updateUserSession({ userId, userRole: role });
+
+    return {
+      isSuccess: true,
+    };
   } catch (error) {
     return {
+      isSuccess: false,
       errors: [error instanceof Error ? error.message : 'An unknown error occurred'],
     };
   }
-
-  return {
-    success: true,
-  };
 }
