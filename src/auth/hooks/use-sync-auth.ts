5879750e9b36d2a0f5e71364c11ed704f0f10339
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-13 01:45:12 +0200

    adjust sync hook to be used on mount

--- src/auth/hooks/use-sync-auth.ts
+++ src/auth/hooks/use-sync-auth.ts
@@ -1,21 +1,16 @@
 import { useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
 import { useAuth } from './use-auth';
-import config from '../config';
 
 export function useSyncAuth() {
   const { syncAuth } = useAuth();
-  const searchParams = useSearchParams();
-  const returnBackUrl = searchParams.get(config.returnBackUrlKey);
 
   useEffect(() => {
-    if (returnBackUrl) {
-      const controller = new AbortController();
+    const controller = new AbortController();
 
-      void syncAuth(controller.signal);
-      return () => {
-        controller.abort();
-      };
-    }
-  }, [returnBackUrl, syncAuth]);
+    void syncAuth(controller.signal);
+
+    return () => {
+      controller.abort();
+    };
+  }, [syncAuth]);
 }
