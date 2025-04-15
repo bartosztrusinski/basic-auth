f1e85fae54be3a3c6256d8516189e68dde964d96
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-15 19:31:43 +0200

    create schemas for oauth fetches

--- src/schemas.ts
+++ src/schemas.ts
@@ -24,4 +24,19 @@ const editProfileSchema = z.object({
   }),
 });
 
-export { signupSchema, loginSchema, editProfileSchema };
+const tokenSchema = z.object({
+  token_type: z.string(),
+  access_token: z.string(),
+  expires_in: z.number(),
+  refresh_token: z.string(),
+  scope: z.string(),
+});
+
+const discordUserSchema = z.object({
+  id: z.string(),
+  username: z.string(),
+  global_name: z.string(),
+  email: z.string().email(),
+});
+
+export { signupSchema, loginSchema, editProfileSchema, tokenSchema, discordUserSchema };
