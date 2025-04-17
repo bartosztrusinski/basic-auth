8cfe0904aa449bf589ce640f67a0da2924e23c5d
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-17 23:23:15 +0200

    use form for oauth log in action

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -7,36 +7,40 @@ export function LoginForm() {
   const [state, action, isPending] = useActionState(logIn, { isSuccess: false });
 
   return (
-    <form action={action} className='flex flex-col gap-5'>
-      <button className='rounded border-2 border-white p-1' onClick={() => oAuthLogIn('discord')}>
-        Discord
-      </button>
-      <input
-        type='email'
-        name='email'
-        required
-        placeholder='Email'
-        autoComplete='email'
-        className='rounded bg-white px-2 py-1 text-base text-black'
-      />
-      <input
-        type='password'
-        name='password'
-        required
-        placeholder='********'
-        autoComplete='current-password'
-        className='rounded bg-white px-2 py-1 text-base text-black'
-      />
-      {state.errors && (
-        <div className='text-sm font-light text-red-500'>
-          {state.errors.map((error, index) => (
-            <p key={index}>{error}</p>
-          ))}
-        </div>
-      )}
-      <button disabled={isPending} className='rounded border-2 border-white p-1'>
-        {isPending ? 'Submitting...' : 'Log In'}
-      </button>
-    </form>
+    <>
+      <div className='grid grid-flow-col gap-2'>
+        <form action={oAuthLogIn.bind(null, 'discord')}>
+          <button className='w-full rounded bg-zinc-800 p-2 font-bold shadow-lg'>Discord</button>
+        </form>
+      </div>
+      <form action={action} className='flex flex-col gap-5'>
+        <input
+          type='email'
+          name='email'
+          required
+          placeholder='Email'
+          autoComplete='email'
+          className='rounded bg-white px-2 py-1 text-base text-black'
+        />
+        <input
+          type='password'
+          name='password'
+          required
+          placeholder='********'
+          autoComplete='current-password'
+          className='rounded bg-white px-2 py-1 text-base text-black'
+        />
+        {state.errors && (
+          <div className='text-sm font-light text-red-500'>
+            {state.errors.map((error, index) => (
+              <p key={index}>{error}</p>
+            ))}
+          </div>
+        )}
+        <button disabled={isPending} className='rounded border-2 border-white p-1'>
+          {isPending ? 'Submitting...' : 'Log In'}
+        </button>
+      </form>
+    </>
   );
 }
