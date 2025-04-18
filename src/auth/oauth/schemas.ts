7d5223899c025259cbf3a32549bebec1a49d0e25
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-04-18 20:49:07 +0200

    add full discord user schema

--- src/auth/oauth/schemas.ts
+++ src/auth/oauth/schemas.ts
@@ -8,12 +8,23 @@ const oAuthTokenSchema = z.object({
   scope: z.string(),
 });
 
-// TODO add more fields to the user schema
 const discordUserSchema = z.object({
   id: z.string(),
-  username: z.string(),
-  global_name: z.string(),
   email: z.string().email(),
+  username: z.string(),
+  discriminator: z.string(),
+  global_name: z.string().nullable(),
+  avatar: z.string().nullable(),
+  bot: z.boolean().optional(),
+  system: z.boolean().optional(),
+  mfa_enabled: z.boolean().optional(),
+  banner: z.string().nullish(),
+  accent_color: z.number().int().nullish(),
+  locale: z.string().optional(),
+  verified: z.boolean().optional(),
+  flags: z.number().int().optional(),
+  premium_type: z.number().int().optional(),
+  public_flags: z.number().int().optional(),
 });
 
 export { oAuthTokenSchema, discordUserSchema };
