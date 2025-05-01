c3d48dcb5fda2070af992629cf467b1b301fb098
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-05-01 02:37:01 +0200

    add close button functionality and change icons

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -1,29 +1,54 @@
+'use client';
+
+import { useState } from 'react';
+
 type Variant = 'default' | 'success' | 'error';
 
 type Props = {
   message: string | string[];
   variant?: Variant;
+  onClose?: () => void;
 };
 
 const variants: Record<Variant, { classes: string; accentChar: string }> = {
   default: { classes: 'border-zinc-600', accentChar: '🔐' },
-  success: { classes: 'border-green-500 text-green-500', accentChar: '✓' },
-  error: { classes: 'border-red-500 text-red-500', accentChar: '⨉' },
+  success: { classes: 'border-green-500 text-green-500', accentChar: '✅' },
+  error: { classes: 'border-red-500 text-red-500', accentChar: '❌' },
 };
 
-export function Alert({ message, variant = 'default' }: Props) {
+export function Alert({ message, variant = 'default', onClose }: Props) {
+  const [isVisible, setIsVisible] = useState(true);
   const { classes, accentChar } = variants[variant];
 
+  if (!isVisible) {
+    return null;
+  }
+
+  function handleClose() {
+    setIsVisible(false);
+    onClose?.();
+  }
+
   return (
-    <div className={`flex items-center gap-2 rounded border p-3 ${classes}`}>
-      <span className='text-lg font-bold leading-none'>{accentChar}</span>
-      <span className='text-sm font-light'>
-        {Array.isArray(message) ? (
-          message.map((err, index) => <p key={index}>{err}</p>)
-        ) : (
-          <p>{message}</p>
-        )}
-      </span>
+    <div
+      className={`group flex items-start justify-between gap-2 rounded border p-2 text-sm ${classes}`}
+    >
+      <div className='flex items-start gap-2'>
+        <span>{accentChar}</span>
+        <span className='font-light'>
+          {Array.isArray(message) ? (
+            message.map((err, index) => <p key={index}>{err}</p>)
+          ) : (
+            <p>{message}</p>
+          )}
+        </span>
+      </div>
+      <button
+        onClick={handleClose}
+        className='size-5 shrink-0 rounded bg-red-500 bg-opacity-0 text-red-500 opacity-0 transition-all duration-100 hover:bg-opacity-20 group-hover:opacity-100'
+      >
+        ⨉
+      </button>
     </div>
   );
 }
