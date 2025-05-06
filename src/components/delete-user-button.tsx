db215269fa8a981cacb32752641569661a3c5a65
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-05-06 14:38:34 +0200

    use dialog to confirm user deletion

--- src/components/delete-user-button.tsx
+++ src/components/delete-user-button.tsx
@@ -2,18 +2,43 @@
 
 import { useActionState } from 'react';
 import { deleteUser } from '@/auth/actions';
+import { Dialog } from '@/components/dialog';
 
 export function DeleteUserButton() {
   const [, action, isPending] = useActionState(deleteUser, null);
 
   return (
-    <form action={action}>
-      <button
-        disabled={isPending}
-        className='w-full rounded bg-red-600 p-4 py-2 shadow-lg shadow-red-900 disabled:cursor-not-allowed disabled:opacity-50'
-      >
-        Delete Account
-      </button>
-    </form>
+    <Dialog
+      trigger={
+        <button className='w-full rounded bg-red-600 p-4 py-2 shadow-lg shadow-red-900 disabled:cursor-not-allowed disabled:opacity-50'>
+          Delete Account
+        </button>
+      }
+    >
+      {(closeDialog) => {
+        return (
+          <div className='flex max-w-sm flex-col gap-1 rounded-md border border-zinc-600 bg-zinc-900 p-4 text-zinc-50'>
+            <button
+              onClick={closeDialog}
+              className='absolute right-3 top-3 size-7 rounded bg-red-500 bg-opacity-0 text-red-500 transition-opacity duration-100 hover:bg-opacity-20'
+            >
+              ⨉
+            </button>
+            <h2 className='text-base font-semibold'>Delete Account</h2>
+            <p className='text-zinc-400'>
+              This action is irreversible and will delete all your data.
+            </p>
+            <form action={action} className='text-end'>
+              <button
+                disabled={isPending}
+                className='rounded bg-red-600 p-4 py-1 shadow disabled:cursor-not-allowed disabled:opacity-50'
+              >
+                {isPending ? 'Deleting...' : 'Delete'}
+              </button>
+            </form>
+          </div>
+        );
+      }}
+    </Dialog>
   );
 }
