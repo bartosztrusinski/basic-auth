9ea1125a6995bd261a306c4699416f38f786bf51
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-05-07 16:53:36 +0200

    adjust db reads to remove expired records

--- src/db/util.ts
+++ src/db/util.ts
@@ -1,30 +1,40 @@
 import 'server-only';
 import fs from 'node:fs/promises';
 
-type Callback<T> = (data: T[]) => T[];
+type Data = Record<string, unknown> & {
+  expirationTime?: number;
+};
+
+type Callback<D extends Data> = (data: D[]) => D[];
 
 const DATA_PATH = `${process.cwd()}/src/data`;
 
-export function createTable<T>(
+export function createTable<D extends Data>(
   filename: string,
-): [() => Promise<T[]>, (callback: Callback<T>) => Promise<void>] {
-  const readTable = () => readDb<T>(filename);
-  const writeTable = (callback: Callback<T>) => writeToDb(filename, callback);
+): [() => Promise<D[]>, (callback: Callback<D>) => Promise<void>] {
+  const readTable = () => readDb<D>(filename);
+  const writeTable = (callback: Callback<D>) => writeToDb(filename, callback);
 
   return [readTable, writeTable];
 }
 
-async function readDb<T>(filename: string): Promise<T[]> {
+async function readDb<D extends Data>(filename: string): Promise<D[]> {
   try {
-    const data = await fs.readFile(`${DATA_PATH}/${filename}`, 'utf-8');
-    return JSON.parse(data) as T[];
+    const rawData = await fs.readFile(`${DATA_PATH}/${filename}`, 'utf-8');
+    const data = JSON.parse(rawData) as D[];
+    const now = Date.now();
+    const nonExpiredData = data.filter((item) =>
+      item.expirationTime ? item.expirationTime > now : true,
+    );
+
+    return nonExpiredData;
   } catch {
-    return [] as T[];
+    return [] as D[];
   }
 }
 
-async function writeToDb<T>(filename: string, callback: Callback<T>): Promise<void> {
-  const data = await readDb<T>(filename);
+async function writeToDb<D extends Data>(filename: string, callback: Callback<D>): Promise<void> {
+  const data = await readDb<D>(filename);
   const newData = callback(data);
   await fs.writeFile(`${DATA_PATH}/${filename}`, JSON.stringify(newData, null, 2));
 }
