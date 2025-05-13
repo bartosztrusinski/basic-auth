79356033a144d75df1c591ba570a03f231078366
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-05-13 19:54:10 +0200

    show password set form in dialog

--- src/components/add-password-form.tsx
+++ src/components/add-password-form.tsx
@@ -1,31 +1,22 @@
 'use client';
 
-import { type FormEvent, useActionState, useState, useTransition } from 'react';
+import { type FormEvent, useActionState, useRef, useState, useTransition } from 'react';
 import { toast } from 'sonner';
 import { type User } from '@/db';
 import { addPassword } from '@/actions';
 import { Alert } from '@/components/alert';
+import { Dialog } from '@/components/dialog';
 import { getAuthMessage } from '@/auth/message';
 
 type Props = {
   email: User['email'];
 };
 
-// TODO dialog
 export function AddPasswordForm({ email }: Props) {
-  const [isFormVisible, setIsFormVisible] = useState(false);
+  const formRef = useRef<HTMLFormElement>(null);
   const [errors, setErrors] = useState<string | string[] | null>(null);
   const [isPending, startTransition] = useTransition();
   const [state, action, isActionPending] = useActionState(addPassword, { isSuccess: false });
-
-  function openForm() {
-    setIsFormVisible(true);
-  }
-
-  function closeForm() {
-    setIsFormVisible(false);
-    setErrors(null);
-  }
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     const formData = new FormData(event.currentTarget);
@@ -39,6 +30,7 @@ export function AddPasswordForm({ email }: Props) {
       if (isSuccess) {
         const { message } = getAuthMessage('password-set');
         toast.success(message);
+        formRef.current?.reset();
       }
 
       if (errors) {
@@ -47,68 +39,82 @@ export function AddPasswordForm({ email }: Props) {
     });
   }
 
-  if (!isFormVisible) {
-    return (
-      <button onClick={openForm} className='w-full rounded border border-zinc-500 p-2'>
-        Add Password
-      </button>
-    );
+  function handleClose() {
+    setErrors(null);
+    formRef.current?.reset();
   }
 
   return (
-    <form action={action} onSubmit={handleSubmit} className='flex flex-col gap-3'>
-      <input
-        type='email'
-        id='email'
-        name='email'
-        required
-        placeholder='Email'
-        autoComplete='email'
-        className='hidden'
-        defaultValue={email}
-      />
-      <input
-        type='password'
-        id='password'
-        name='password'
-        required
-        placeholder='Password'
-        autoComplete='new-password'
-        className='rounded bg-white px-2 py-1 text-base text-black'
-      />
-      <input
-        type='password'
-        id='confirmPassword'
-        name='confirmPassword'
-        required
-        placeholder='Confirm Password'
-        autoComplete='new-password'
-        className='rounded bg-white px-2 py-1 text-base text-black'
-      />
+    <Dialog
+      trigger={<button className='w-full rounded border border-zinc-500 p-2'>Add Password</button>}
+      onClose={handleClose}
+    >
+      {(closeDialog) => {
+        return (
+          <div className='flex max-w-sm flex-col gap-2 rounded-md border border-zinc-700 bg-zinc-900 p-4 text-zinc-50'>
+            <button
+              onClick={closeDialog}
+              className='absolute right-3 top-3 size-7 rounded bg-red-500 bg-opacity-0 text-red-500 transition-opacity duration-100 hover:bg-opacity-20'
+            >
+              ⨉
+            </button>
+            <h2 className='font-medium'>Set Password</h2>
+            <p className='pb-1 text-sm text-zinc-400'>
+              After setting a password, you will be able to log in with your email and password.
+            </p>
+            <form
+              ref={formRef}
+              action={action}
+              onSubmit={handleSubmit}
+              className='flex flex-col gap-3'
+            >
+              <input
+                type='email'
+                id='email'
+                name='email'
+                required
+                placeholder='Email'
+                autoComplete='email'
+                className='hidden'
+                defaultValue={email}
+              />
+              <input
+                type='password'
+                id='password'
+                name='password'
+                required
+                placeholder='Password'
+                autoComplete='new-password'
+                className='rounded bg-white px-2 py-1 text-base text-black'
+              />
+              <input
+                type='password'
+                id='confirmPassword'
+                name='confirmPassword'
+                required
+                placeholder='Confirm Password'
+                autoComplete='new-password'
+                className='rounded bg-white px-2 py-1 text-base text-black'
+              />
 
-      {errors && <Alert variant='error' message={errors} />}
-      {state.errors && (
-        <noscript className='rounded border border-red-900 bg-red-950 p-2 text-sm text-red-400'>
-          {state.errors}
-        </noscript>
-      )}
+              {errors && <Alert variant='error' message={errors} />}
+              {state.errors && (
+                <noscript className='rounded border border-red-900 bg-red-950 p-2 text-sm text-red-400'>
+                  {state.errors}
+                </noscript>
+              )}
 
-      <div className='flex gap-2'>
-        <button
-          type='submit'
-          disabled={isPending || isActionPending}
-          className='w-full rounded border border-zinc-500 p-2'
-        >
-          {isPending || isActionPending ? 'Submitting...' : 'Set Password'}
-        </button>
-        <button
-          type='button'
-          onClick={closeForm}
-          className='w-10 shrink-0 rounded border border-zinc-500 p-2'
-        >
-          ⨉
-        </button>
-      </div>
-    </form>
+              <button
+                type='submit'
+                disabled={isPending || isActionPending}
+                className='rounded border border-zinc-500 p-2'
+              >
+                {isPending || isActionPending ? 'Submitting...' : 'Set Password'}
+              </button>
+            </form>
+          </div>
+        );
+      }}
+    </Dialog>
   );
 }
