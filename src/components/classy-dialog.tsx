3e45ff2a2c56b95c4cd712d70969e329dfb93735
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-05-17 01:01:26 +0200

    create styled dialog component

---
+++ src/components/classy-dialog.tsx
@@ -0,0 +1,26 @@
+import { Dialog, type DialogProps } from '@/components/dialog';
+
+type Props = DialogProps & {
+  heading?: string;
+  description?: string;
+};
+
+export function ClassyDialog({ children, heading, description, ...props }: Props) {
+  return (
+    <Dialog {...props}>
+      {(closeDialog, openDialog) => (
+        <div className='flex max-w-sm flex-col gap-2 rounded-md border border-zinc-700 bg-zinc-900 p-4 text-zinc-50'>
+          {heading && <h2 className='pr-5 font-medium'>{heading}</h2>}
+          {description && <p className='pb-1 text-sm text-zinc-400'>{description}</p>}
+          {typeof children === 'function' ? children(closeDialog, openDialog) : children}
+          <button
+            onClick={closeDialog}
+            className='absolute right-2 top-2 size-7 rounded bg-red-500 bg-opacity-0 text-red-500 transition-opacity duration-100 hover:bg-opacity-20'
+          >
+            ⨉
+          </button>
+        </div>
+      )}
+    </Dialog>
+  );
+}
