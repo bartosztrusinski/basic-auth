114c75cefca6f4e6512bd94fa5f92c679a405fb3
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-05-16 22:25:40 +0200

    use otp input component

--- src/components/enable-two-factor-form.tsx
+++ src/components/enable-two-factor-form.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { type FormEvent, useActionState, useState, useTransition } from 'react';
+import { type FormEvent, useActionState, useRef, useState, useTransition } from 'react';
 import Image from 'next/image';
 import { toast } from 'sonner';
 import { type TwoFactorSetup } from '@/db';
 import { enableTwoFactorAuth } from '@/actions';
 import { Alert } from '@/components/alert';
+import { OtpInput } from '@/components/otp-input';
 import { getAuthMessage } from '@/auth/message';
 
 type Props = {
@@ -19,6 +20,7 @@ export function EnableTwoFactorForm({ secret, qrCode }: Props) {
   const [state, action, isActionPending] = useActionState(enableTwoFactorAuth, {
     isSuccess: false,
   });
+  const formRef = useRef<HTMLFormElement>(null);
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     const formData = new FormData(event.currentTarget);
@@ -50,16 +52,39 @@ export function EnableTwoFactorForm({ secret, qrCode }: Props) {
         height={256}
         className='rounded'
       />
-      <form action={action} onSubmit={handleSubmit} className='flex flex-col gap-3 self-stretch'>
-        {/* TODO OTP input */}
-        <input
+      <form
+        ref={formRef}
+        action={action}
+        onSubmit={handleSubmit}
+        className='flex flex-col gap-3 self-stretch'
+      >
+        <OtpInput
           id='token'
           name='token'
           required
-          placeholder='123456'
-          autoComplete='one-time-code'
-          className='rounded-sm bg-white px-2 py-1 text-base text-black'
-        />
+          autoFocus
+          className='peer rounded'
+          focusClassName='outline-2 outline-zinc-400'
+          containerClassName='mx-auto flex w-min items-center justify-between gap-1 p-0.5'
+          onComplete={() => formRef.current?.requestSubmit()}
+        >
+          {(slots) =>
+            slots.map((slot, slotIndex) => (
+              <div
+                key={slotIndex}
+                className={`flex size-10 place-content-center place-items-center rounded-sm bg-zinc-800 text-center text-lg text-zinc-50 outline-2 outline-zinc-400 ${slot.isActive ? 'peer-focus:outline' : ''}`}
+              >
+                {slot.value}
+                {slot.hasCaret && (
+                  <span className='pointer-events-none h-[1.5ch] w-[0.2ch] animate-caret-blink bg-current'></span>
+                )}
+                {slot.placeholder && (
+                  <span className='pointer-events-none text-zinc-500'>{slot.placeholder}</span>
+                )}
+              </div>
+            ))
+          }
+        </OtpInput>
 
         {errors && <Alert variant='error' message={errors} />}
         {state.errors && (
