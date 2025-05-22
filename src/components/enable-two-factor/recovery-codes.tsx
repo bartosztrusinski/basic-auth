8fc9f7d0548a97b74cdb059c9b04d7365476acf4
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-05-22 15:41:21 +0200

    add button to copy codes to clipboard

--- src/components/enable-two-factor/recovery-codes.tsx
+++ src/components/enable-two-factor/recovery-codes.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
 import { type TwoFactorData } from '@/components/enable-two-factor';
 
 type Props = {
@@ -5,23 +8,51 @@ type Props = {
   onClose?: () => void;
 };
 
-function formatRecoveryCode(code: string, { delimiter = '-', blockLength = 4 } = {}) {
+function formatCode(code: string, { delimiter = '-', blockLength = 4 } = {}) {
   const regex = new RegExp(`.{${blockLength}}(?!$)`, 'g');
   return code.replace(regex, `$&${delimiter}`);
 }
 
 export function RecoveryCodes({ recoveryCodes, onClose }: Props) {
+  const [isCopied, setIsCopied] = useState(false);
+  const timeoutId = useRef<number>();
+
+  async function copyCodesToClipboard() {
+    await navigator.clipboard.writeText(recoveryCodes.join('\n'));
+
+    setIsCopied(true);
+    clearCurrentTimeout();
+
+    timeoutId.current = window.setTimeout(() => {
+      setIsCopied(false);
+    }, 1200);
+  }
+
+  function clearCurrentTimeout() {
+    window.clearTimeout(timeoutId.current);
+  }
+
+  useEffect(() => {
+    return clearCurrentTimeout;
+  }, []);
+
   return (
     <>
       <code className='flex flex-row flex-wrap items-center justify-center gap-x-4 gap-y-2 py-2'>
         {recoveryCodes.map((code) => (
           <div className='rounded bg-zinc-800 px-2 py-1' key={code}>
-            {formatRecoveryCode(code)}
+            {formatCode(code)}
           </div>
         ))}
       </code>
       <button
-        className='rounded border border-zinc-700 p-2 text-sm shadow disabled:cursor-not-allowed disabled:opacity-50'
+        className={`rounded border border-zinc-700 p-2 text-sm shadow transition-colors duration-200 ease-in-out ${isCopied ? 'bg-green-700' : ''}`}
+        onClick={copyCodesToClipboard}
+      >
+        {isCopied ? 'Copied!' : 'Copy to Clipboard'}
+      </button>
+      <button
+        className='rounded bg-zinc-800 p-2 text-sm shadow disabled:cursor-not-allowed disabled:opacity-50'
         onClick={onClose}
       >
         I have saved my recovery codes
