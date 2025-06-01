928712c7016816e68cfcd2dd5a457c72f7e3f3d0
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-06-01 23:51:04 +0200

    make dialog simpler and flexible

--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -5,37 +5,53 @@ import {
   useRef,
   useState,
   cloneElement,
+  isValidElement,
+  createContext,
+  useContext,
   type MouseEvent,
   type ReactElement,
   type ReactNode,
+  type Dispatch,
+  type SetStateAction,
+  type MouseEventHandler,
 } from 'react';
+import { callAll } from '@/util';
 
-export type DialogProps = {
-  children: ReactNode | ((closeDialog: () => void) => ReactNode);
-  trigger?: ReactElement<Record<string, unknown> & { onClick?: (event: MouseEvent) => void }>;
-  isOpen?: boolean;
-  onClose?: () => void;
+type DialogContext = {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+type DialogContentProps = {
+  children: ReactNode;
   shouldCloseOnBackdropClick?: boolean;
 };
 
-export function Dialog({
-  children,
-  trigger,
-  isOpen: externalIsOpen,
-  onClose,
-  shouldCloseOnBackdropClick = true,
-}: DialogProps) {
+type DialogButtonProps = {
+  children: ReactElement<Record<string, unknown> & { onClick?: MouseEventHandler }>;
+};
+
+const DialogContext = createContext<DialogContext>({
+  isOpen: false,
+  setIsOpen: () => null,
+});
+
+function useDialog(): DialogContext {
+  return useContext(DialogContext);
+}
+
+function Dialog({ children }: { children: ReactNode }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return <DialogContext.Provider value={{ isOpen, setIsOpen }}>{children}</DialogContext.Provider>;
+}
+
+function DialogContent({ children, shouldCloseOnBackdropClick = true }: DialogContentProps) {
   const dialogRef = useRef<HTMLDialogElement>(null);
-  const [internalIsOpen, setInternalIsOpen] = useState(false);
-  const isOpen = externalIsOpen ?? internalIsOpen;
-  const isControlled = externalIsOpen !== undefined;
+  const { isOpen, setIsOpen } = useDialog();
 
   function closeDialog() {
-    onClose?.();
-
-    if (!isControlled) {
-      setInternalIsOpen(false);
-    }
+    setIsOpen(false);
   }
 
   function handleBackdropClick(event: MouseEvent<HTMLDialogElement>) {
@@ -54,46 +70,44 @@ export function Dialog({
     }
   }, [isOpen]);
 
-  useEffect(() => {
-    if (isControlled && trigger) {
-      console.warn(
-        'You provided both `trigger` and `isOpen` props. This will not render `trigger`. For controlled dialogs, manage state with `isOpen` prop. For uncontrolled dialogs, use `trigger` prop.',
-      );
-    }
-  }, [isControlled, trigger]);
-
-  useEffect(() => {
-    if (isControlled && !onClose) {
-      console.warn(
-        'You provided `isOpen` prop without `onClose`. This will not close the dialog when `isOpen` changes. Provide `onClose` handler to manage dialog state.',
-      );
-    }
-  }, [isControlled, onClose]);
+  if (!isOpen) {
+    return null;
+  }
 
   return (
-    <>
-      {trigger &&
-        !isControlled &&
-        cloneElement(trigger, {
-          ...trigger.props,
-          onClick: (event: MouseEvent) => {
-            if (trigger.props.onClick) {
-              trigger.props.onClick(event);
-            }
-
-            setInternalIsOpen(true);
-          },
-        })}
-      {isOpen && (
-        <dialog
-          ref={dialogRef}
-          onClose={closeDialog}
-          onMouseDown={handleBackdropClick}
-          className='bg-transparent shadow-lg shadow-neutral-950 backdrop:bg-neutral-950/50 backdrop:backdrop-blur-sm'
-        >
-          {typeof children === 'function' ? children(closeDialog) : children}
-        </dialog>
-      )}
-    </>
+    <dialog
+      ref={dialogRef}
+      onClose={closeDialog}
+      onMouseDown={handleBackdropClick}
+      className='bg-transparent shadow-lg shadow-neutral-950 backdrop:bg-neutral-950/50 backdrop:backdrop-blur-sm'
+    >
+      {children}
+    </dialog>
   );
 }
+
+function DialogOpenButton({ children }: DialogButtonProps) {
+  const { setIsOpen } = useDialog();
+
+  return <DialogButton onClick={() => setIsOpen(true)}>{children}</DialogButton>;
+}
+
+function DialogCloseButton({ children }: DialogButtonProps) {
+  const { setIsOpen } = useDialog();
+
+  return <DialogButton onClick={() => setIsOpen(false)}>{children}</DialogButton>;
+}
+
+function DialogButton({ children, onClick }: { onClick: MouseEventHandler } & DialogButtonProps) {
+  if (!isValidElement(children)) {
+    throw new Error(`
+      ${DialogButton.name} expects a single React element as its child.`);
+  }
+
+  return cloneElement(children, {
+    ...children.props,
+    onClick: callAll(children.props.onClick, onClick),
+  });
+}
+
+export { Dialog, DialogContent, DialogOpenButton, DialogCloseButton };
