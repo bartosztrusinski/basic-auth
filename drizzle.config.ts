ce4c248905bac7772b7ef5b7e098b97b6d0941a2
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-06-06 19:09:28 +0200

    use snake case in tables; colocate migrations folder

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'drizzle-kit';
 import { env } from '@/env';
 
 export default defineConfig({
-  out: './drizzle',
+  out: './src/db/migrations',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: env.DATABASE_URL,
   },
+  casing: 'snake_case',
 });
