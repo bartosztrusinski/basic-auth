85025f3fe02b10f730dc3664eac4a9e5a5d4a674
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-06-07 15:59:38 +0200

    add drizzle schemas for tokens, 2FA setups and recovery codes

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -10,6 +10,10 @@ import {
 } from 'drizzle-orm/pg-core';
 import { OAuthProviderEnum } from '@/auth/config/providers';
 
+const userIdReference = uuid()
+  .notNull()
+  .references(() => usersTable.id, { onDelete: 'cascade' });
+
 export const rolesEnum = pgEnum('roles', ['user', 'admin']);
 export const usersTable = pgTable(
   'users',
@@ -31,13 +35,10 @@ export const accountsTable = pgTable(
   {
     provider: providersEnum().notNull(),
     providerAccountId: varchar({ length: 255 }).notNull(),
-    userId: uuid()
-      .notNull()
-      .references(() => usersTable.id, { onDelete: 'cascade' }),
+    userId: userIdReference,
   },
   (table) => [
     primaryKey({ columns: [table.provider, table.providerAccountId] }),
-    unique().on(table.provider, table.providerAccountId),
     unique().on(table.userId, table.provider),
   ],
 );
@@ -45,7 +46,33 @@ export const accountsTable = pgTable(
 export const sessionsTable = pgTable('sessions', {
   id: varchar({ length: 43 }).primaryKey(),
   expiresAt: timestamp().notNull(),
-  userId: uuid()
-    .notNull()
-    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  userId: userIdReference,
 });
+
+export const verificationTokensTable = pgTable('verification_tokens', {
+  token: varchar({ length: 86 }).primaryKey(),
+  expiresAt: timestamp().notNull(),
+  userId: userIdReference,
+});
+
+export const twoFactorAttemptsTable = pgTable('two_factor_attempts', {
+  token: varchar({ length: 86 }).primaryKey(),
+  expiresAt: timestamp().notNull(),
+  userId: userIdReference,
+});
+
+export const twoFactorSetupsTable = pgTable('two_factor_setups', {
+  secret: varchar({ length: 64 }).notNull(),
+  expiresAt: timestamp().notNull(),
+  userId: userIdReference,
+});
+
+export const recoveryCodesTable = pgTable(
+  'recovery_codes',
+  {
+    code: varchar({ length: 97 }),
+    usedAt: timestamp(),
+    userId: userIdReference,
+  },
+  (table) => [primaryKey({ columns: [table.code, table.userId] })],
+);
