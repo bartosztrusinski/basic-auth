348dd5ee8c5e40276bab4e2c5961d6d341b73b64
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-06-10 16:28:43 +0200

    remove mock db files

--- src/db/util.ts
+++ src/db/util.ts
@@ -1,40 +1,40 @@
-import 'server-only';
-import fs from 'node:fs/promises';
+// import 'server-only';
+// import fs from 'node:fs/promises';
 
-type Data = Record<string, unknown> & {
-  expirationTime?: number;
-};
+// type Data = Record<string, unknown> & {
+//   expirationTime?: number;
+// };
 
-type Callback<D extends Data> = (data: D[]) => D[];
+// type Callback<D extends Data> = (data: D[]) => D[];
 
-const DATA_PATH = `${process.cwd()}/src/data`;
+// const DATA_PATH = `${process.cwd()}/src/data`;
 
-export function createTable<D extends Data>(
-  filename: string,
-): [() => Promise<D[]>, (callback: Callback<D>) => Promise<void>] {
-  const readTable = () => readDb<D>(filename);
-  const writeTable = (callback: Callback<D>) => writeToDb(filename, callback);
+// export function createTable<D extends Data>(
+//   filename: string,
+// ): [() => Promise<D[]>, (callback: Callback<D>) => Promise<void>] {
+//   const readTable = () => readDb<D>(filename);
+//   const writeTable = (callback: Callback<D>) => writeToDb(filename, callback);
 
-  return [readTable, writeTable];
-}
+//   return [readTable, writeTable];
+// }
 
-async function readDb<D extends Data>(filename: string): Promise<D[]> {
-  try {
-    const rawData = await fs.readFile(`${DATA_PATH}/${filename}`, 'utf-8');
-    const data = JSON.parse(rawData) as D[];
-    const now = Date.now();
-    const nonExpiredData = data.filter((item) =>
-      item.expirationTime ? item.expirationTime > now : true,
-    );
+// async function readDb<D extends Data>(filename: string): Promise<D[]> {
+//   try {
+//     const rawData = await fs.readFile(`${DATA_PATH}/${filename}`, 'utf-8');
+//     const data = JSON.parse(rawData) as D[];
+//     const now = Date.now();
+//     const nonExpiredData = data.filter((item) =>
+//       item.expirationTime ? item.expirationTime > now : true,
+//     );
 
-    return nonExpiredData;
-  } catch {
-    return [] as D[];
-  }
-}
+//     return nonExpiredData;
+//   } catch {
+//     return [] as D[];
+//   }
+// }
 
-async function writeToDb<D extends Data>(filename: string, callback: Callback<D>): Promise<void> {
-  const data = await readDb<D>(filename);
-  const newData = callback(data);
-  await fs.writeFile(`${DATA_PATH}/${filename}`, JSON.stringify(newData, null, 2));
-}
+// async function writeToDb<D extends Data>(filename: string, callback: Callback<D>): Promise<void> {
+//   const data = await readDb<D>(filename);
+//   const newData = callback(data);
+//   await fs.writeFile(`${DATA_PATH}/${filename}`, JSON.stringify(newData, null, 2));
+// }
