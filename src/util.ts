cef37a311a7175d1c0376350e34a4992457abce4
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-06-10 16:07:37 +0200

    create util for expiration date

--- src/util.ts
+++ src/util.ts
@@ -7,3 +7,7 @@ export function formatCode(code: string, { delimiter = '-', blockLength = 4 } = 
   const regex = new RegExp(`.{${blockLength}}(?!$)`, 'g');
   return code.replace(regex, `$&${delimiter}`);
 }
+
+export function createExpirationDate(secondsToExpiration: number): Date {
+  return new Date(Date.now() + secondsToExpiration * 1000);
+}
