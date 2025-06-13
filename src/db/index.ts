cf56983d1682d4260d169f2b1136fb2a683da855
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-06-13 17:51:00 +0200

    use snake casing for queries

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,4 +3,4 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { env } from '@/env';
 import * as schema from './schema';
 
-export const db = drizzle(env.DATABASE_URL, { schema });
+export const db = drizzle(env.DATABASE_URL, { schema, casing: 'snake_case' });
