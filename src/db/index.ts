f396077e776d7edd6ef92f39cb5646c1c84f3f23
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-07-01 19:48:16 +0200

    create type to support db transactions

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,3 +4,6 @@ import { env } from '@/env';
 import * as schema from './schema';
 
 export const db = drizzle(env.DATABASE_URL, { schema, casing: 'snake_case' });
+
+type Transaction = Parameters<Parameters<typeof db.transaction>[0]>[0];
+export type DbInstance = typeof db | Transaction;
