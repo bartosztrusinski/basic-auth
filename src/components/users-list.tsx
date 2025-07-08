556170b7d5f8d93a24be8d34bad9a75f18711667
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-07-08 22:24:57 +0200

    show users date created instead of email

--- src/components/users-list.tsx
+++ src/components/users-list.tsx
@@ -3,11 +3,16 @@ import { Protect } from '@/auth/components/protect';
 
 export async function UsersList() {
   const users = await getUsers();
+  const dateFormatter = new Intl.DateTimeFormat('pl-PL', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
 
   return (
     <Protect
       when={(user) => user.role !== 'admin'}
-      fallback={<p className='text-center'>You do not have permission to view this data</p>}
+      fallback={<p className='text-center'>You do not have permissions to view user data</p>}
     >
       <ul className='max-h-80 space-y-3 overflow-y-auto'>
         {users.map((user) => (
@@ -16,7 +21,9 @@ export async function UsersList() {
               {user.name}
               <strong className='font-mono font-medium text-primary-500'>{user.role}</strong>
             </div>
-            <span className='text-sm'>{user.email}</span>
+            <span className='text-sm text-neutral-500'>
+              Created on {dateFormatter.format(user.createdAt)}
+            </span>
           </li>
         ))}
       </ul>
